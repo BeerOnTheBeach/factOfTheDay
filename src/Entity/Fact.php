d277ff05f4c0fc122b4791640347b1dc650cebf3
Gero <g.proebster@die-lobby.de>
2025-07-18 13:59:14 +0200

    Initital commit

---
+++ src/Entity/Fact.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FactRepository;
+use DateTime;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FactRepository::class)]
+class Fact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private string $label;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private string $body;
+
+    #[ORM\ManyToOne(inversedBy: 'facts')]
+    private User $author;
+
+    #[ORM\Column(nullable: true)]
+    private DateTime $published;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): static
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(User $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPublished(): DateTime
+    {
+        return $this->published;
+    }
+
+    public function setPublished(DateTime $published): static
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+}
